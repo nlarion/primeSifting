61915238a98503eb9c9ea82e20a0df65826b9701
Neil Larion <neil.larion@gmail.com>
2016-01-20 21:18:32 -0800

    Rename master.js to scripts.js

---
+++ js/scripts.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    var messageInput =$("input#primeSifting").val();
+    var result = pigLatinSentence(messageInput);
+    $("#result").text(result);
+    $("#result").show();
+    event.preventDefault();
+  });
+});
+
