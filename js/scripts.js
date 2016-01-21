988aec20cb282bf43a65d0625f2c8eb950a3ae16
Neil Larion <neil.larion@gmail.com>
2016-01-21 00:30:35 -0800

    Started primes function logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,46 @@
 $(document).ready(function() {
   $("form").submit(function(event) {
-    var messageInput =$("input#primeSifting").val();
-    var result = pigLatinSentence(messageInput);
+    var input =$("input#primeSifting").val();
+    var result = primes(2, input);
     $("#result").text(result);
     $("#result").show();
     event.preventDefault();
   });
 });
 
+function primes(num){
+  var arr = [];
+  for (var i = 2; i < num+1; i++) {
+    arr.push(i);
+  };
+  console.log(arr);
+  for (var i = 0; i < arr.length; i++){
+    //console.log("devisor: "+arr[i]);
+    for (var j = 0; j < arr[i]; j++){
+        if((arr[i]%arr[j]) === 0 && arr[i]!=arr[j]){
+
+        console.log("i "+arr[i]+" j "+arr[j]);
+        arr.splice(j,0);
+
+        }
+  
+    }
+  }
+console.log(arr);
+
+}
+
+
+
+  // function primes(x, y) {
+
+  //   //arr.push(x);
+  //   //arr.concat(x);
+  //   if ( x > y) {
+  //     return -1
+  //   }else {
+  //     if x
+  //     console.log(x);
+  //     return primes(x+1,y);
+  //   }
+  // }
--- spec/spec.js
+++ spec/spec.js
@@ -1,5 +1,8 @@
-describe('addAy',function(){
+describe('primes',function(){
   it("Will return first primtes from 2 to user input interger",function(){
     expect(primes(100)).to.equal("2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 71 73 79 83 89 97");
   });
+  //   it("Will return -1 if number is < 1",function(){
+  //   expect(primes(1)).to.equal(-1);
+  // });
 });
