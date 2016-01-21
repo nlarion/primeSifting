0d70a0cbf97e975adc421f610e3ca98a292b6638
Neil Larion <neil.larion@gmail.com>
2016-01-20 21:12:05 -0800

    Add first test

--- spec/spec.js
+++ spec/spec.js
@@ -1,5 +1,5 @@
 describe('addAy',function(){
-  it("Will add 'ay' to the end of a word that starts with a vowel",function(){
-    expect(addAy("open")).to.equal("openay");
+  it("Will return first primtes from 2 to user input interger",function(){
+    expect(primes(100)).to.equal("2 3 5 7 11 13 17 19 23 29 31 37 41 43 47 53 59 61 71 73 79 83 89 97");
   });
 });
